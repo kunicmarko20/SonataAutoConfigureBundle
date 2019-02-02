4675818952bee75baa84766e9364f977649230cb
kunicmarko20 <kunicmarko20@gmail.com>
2019-02-02 21:23:12 +0000

    drop lowest

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -11,5 +11,3 @@ chmod u+x "${HOME}/bin/phpunit"
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
-
-composer install ${COMPOSER_FLAGS}

7f84b5c08094c15481afb64d642db71c1dc3529c
kunicmarko20 <kunicmarko20@gmail.com>
2019-02-02 21:31:23 +0000

    drop web server

--- .travis/script_behat.sh
+++ .travis/script_behat.sh
@@ -4,5 +4,4 @@ set -ev
 features/Fixtures/Project/bin/console --no-interaction cache:clear --env=test
 features/Fixtures/Project/bin/console doctrine:schema:update --force --env=test
 features/Fixtures/Project/bin/console assets:install
-nohup php -S 127.0.0.1:8000 -t features/Fixtures/Project/public 2>&1 &
 vendor/bin/behat

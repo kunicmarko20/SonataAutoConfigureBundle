26128214bbad0c5e5e1c4ad3add8a2d97d02cff6
kunicmarko20 <kunicmarko20@gmail.com>
2019-02-02 21:00:29 +0000

    change travis file

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+
 set -ev
 
 mkdir --parents "${HOME}/bin"
@@ -11,5 +12,4 @@ chmod u+x "${HOME}/bin/phpunit"
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
 
-if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
-composer install --dev --prefer-dist
+composer install --dev "${COMPOSER_FLAGS}"
